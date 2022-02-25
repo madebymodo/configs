4bd4b591babe4b1bf88ba44059f7582083fb5aac
Adam Di Mario <nev9adam@gmail.com>
2022-02-25 22:41:20 +0100

    chore: refactor

--- packages/init-modo-app/index.mjs
+++ packages/init-modo-app/index.mjs
@@ -9,13 +9,16 @@ import spawn from "cross-spawn";
 const dependencies = ["gitlab:cescoc/eslint-config-modo-base"];
 
 const create = async () => {
-  console.log(`Installing ${chalk.cyan("MODO")} tools...`);
+  console.log(`Init ${chalk.cyan("MODO")} project`);
 
   const hasPackageJson = readFileSync(path.join(process.cwd(), "package.json"));
+
+  console.log(`Checking for existing package.json...`);
 
   if (hasPackageJson) {
     console.log(`Detected existing package.json...`);
   } else {
+    console.log(`No package.json found.`);
     console.log(`Initializing new dummy project...`);
     const packageJson = {
       name: "app-name",
@@ -28,18 +31,6 @@ const create = async () => {
     );
   }
 
-  const prettierConfig = {
-    printWidth: 100,
-    trailingComma: "es5",
-    tabWidth: 2,
-    semi: false,
-    singleQuote: true,
-  };
-  writeFileSync(
-    path.join(process.cwd(), ".prettierrc"),
-    JSON.stringify(prettierConfig, null, 2) + os.EOL
-  );
-
   const command = "npm";
   const args = [
     "install",
@@ -49,6 +40,8 @@ const create = async () => {
     "--loglevel",
     "error",
   ].concat(dependencies);
+
+  console.log(`Installing dependencies...`);
 
   await new Promise((resolve, reject) => {
     const child = spawn(command, args, { stdio: "inherit" });
@@ -63,6 +56,20 @@ const create = async () => {
     });
   });
 
+  console.log(`Writing configs...`);
+
+  const prettierConfig = {
+    printWidth: 100,
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+  };
+  writeFileSync(
+    path.join(process.cwd(), ".prettierrc"),
+    JSON.stringify(prettierConfig, null, 2) + os.EOL
+  );
+
   const eslintConfig = {
     extends: ["modo-base"],
   };
@@ -70,6 +77,8 @@ const create = async () => {
     path.join(process.cwd(), ".eslintrc.json"),
     JSON.stringify(eslintConfig, null, 2) + os.EOL
   );
+
+  console.log(`${chalk.green("Done!")}`);
 };
 
 await create();
